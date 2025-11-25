3c3c1f5eb84aed09690873f636a417e71c215690
toqeer92ahmad-ali <toqeer92ahmad@gmail.com>
2025-11-25 21:08:31 +0500

    complete sign up page

--- api/index.js
+++ api/index.js
@@ -25,5 +25,9 @@ app.use("/api/auth", authRouter);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
-  res.status(statusCode).json({ error: message });
-});
+  return res.status(statusCode).json ({
+    success: false,
+    statusCode,
+    message,
+  });
+  });
