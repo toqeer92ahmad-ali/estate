38e7e6dd182194645e1c40e54fde4cedd950e817
toqeer92ahmad-ali <toqeer92ahmad@gmail.com>
2025-11-28 10:56:35 +0500

    complete image upload functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
