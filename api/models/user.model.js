5c5016404bde5c4664b99aa9b4f3afd915aec61f
toqeer92ahmad-ali <toqeer92ahmad@gmail.com>
2025-11-28 15:52:16 +0500

    complete listing backend

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,26 +1,30 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-    }, 
+      type: String,
+      required: true,
+      unique: true,
+    },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    avatar: {
-        type: String,
-        default: "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?semt=ais_hybrid&w=740&q=80"
+    avatar:{
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     },
-}, { timestamps: true});
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
-
+    
---
+++ api/routes/listing.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { createListing } from "../controllers/listing.controller.js";
+
+const router = express.Router();
+
+router.post("/create", cerifyToken,  createListing);
+
+export default router;
